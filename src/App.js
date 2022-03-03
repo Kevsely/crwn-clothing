98c16656ce9e0497e60375cbada8b2a4a8c5af66
Kevsely <elegis.sossou@yahoo.com>
2022-03-03 08:42:26 +0100

    Unsuccessful try to install sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import HomePage from './homepage.component'
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
