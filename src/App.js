378e21086a3d4fac34803ac6a4c5bfb59941cc5a
Kevsely <elegis.sossou@yahoo.com>
2022-03-06 11:10:48 +0100

    Added basic router functionnality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './components/shop/shop.component';
+
 
 function App() {
-  return (
-    <div>
-      <HomePage/>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route exact path='/' element={<HomePage />} />
+                <Route path='/shop' element={<ShopPage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/shop/shop.component.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class ShopPage extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            section: ""
+        }
+    }
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default ShopPage;
