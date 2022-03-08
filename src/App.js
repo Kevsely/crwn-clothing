97b87fa687099dd7db5f222ec458f34c4fbf874b
Kevsely <elegis.sossou@yahoo.com>
2022-03-08 10:53:57 +0100

    Installed redux, react-redux and react-logger Set Provider (from react-redux) inside index.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import './App.css';
 
