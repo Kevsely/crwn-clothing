0925ad88b4ebe388dc88dfe306f537e62edbf8f4
Kevsely <elegis.sossou@yahoo.com>
2022-03-09 09:47:41 +0100

    Binding Card Dropdown to  Reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import { ReactComponent as ShoppingBag } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = () => (
-    <div className="cart-icon">
+const CartIcon = ({ toggleCardHidden }) => (
+    <div className="cart-icon" onClick={toggleCardHidden}>
         <ShoppingBag className="shopping-icon" />
         <span className="item-count">0</span>
     </div>
 )
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+    toggleCardHidden: () => dispatch(toggleCardHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon);
