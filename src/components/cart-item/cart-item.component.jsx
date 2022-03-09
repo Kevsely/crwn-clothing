0fb2ac114fe8f6b7543d09f602cbac3ced5d0a83
Kevsely <elegis.sossou@yahoo.com>
2022-03-09 23:44:07 +0100

    Filled up the CartDropdown with the CartItems

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./cart-item.styles.scss"
 
-const cartItem = ({ item: {name, imageUrl, price, quantity} }) => (
+const CartItem = ({ item: {name, imageUrl, price, quantity} }) => (
     <div className="cart-item">
         <img src={imageUrl} alt="item" />
         <div className="item-details">
@@ -12,4 +12,4 @@ const cartItem = ({ item: {name, imageUrl, price, quantity} }) => (
     </div>
 )
 
-export default cartItem;
+export default CartItem;
