4912bbe1833364dfc0cd6688a34cecf470979117
Kevsely <elegis.sossou@yahoo.com>
2022-03-09 06:15:50 +0100

    Added Cart Icon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
-
-import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from "../../firebase/firebase.utils";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -20,6 +21,7 @@ const Header = ({ currentUser }) => (
                 <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div> : 
                 <Link className="option" to="/signin">SIGN IN</Link>
             }
+            <CartIcon />
         </div>
     </div>
 );
