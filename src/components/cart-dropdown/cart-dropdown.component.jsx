74b27da446a106cdd5a6dc6638a26eb76de7566b
Kevsely <elegis.sossou@yahoo.com>
2022-03-10 16:10:34 +0100

    Created checkout item

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -9,18 +9,20 @@ import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 
 import { selectCartItems } from "../../redux/cart/cart.selectors";
-
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 const CartDropdown = ({cartItems, dispatch}) => {
     const navigate = useNavigate();
-    console.log(cartItems)
     return (<div className="cart-dropdown">
         <div className="cart-items">{
             cartItems.length ? 
                 cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>) :   
                 <span className="empty-message">Your cart is empty</span>
         }</div>
-        <CustomButton onClick={() => navigate("/checkout")}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            navigate("/checkout")
+            dispatch(toggleCartHidden())
+        }}>GO TO CHECKOUT</CustomButton>
     </div>)
 }
 
