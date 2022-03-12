dcb972936434b2195aeabbc0db7dc125fda042d9
Kevsely <elegis.sossou@yahoo.com>
2022-03-12 08:04:32 +0100

    Binding redux state to the collection page

--- src/pages/collection-page/collection-page.component.jsx
+++ src/pages/collection-page/collection-page.component.jsx
@@ -2,10 +2,14 @@ import "./collection-page.styles.scss";
 
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
 const CollectionPage = () => {
-    const params = useParams();
-    console.log(params);
+    const { collectionId } = useParams();
+    const collection = useSelector(selectCollection(collectionId))
+    console.log(collection)
     return (
     <div className="collection-page">
         <h2>COLLECTION PAGE</h2>
