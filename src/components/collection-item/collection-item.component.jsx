fc10bee82772a6680fe82d93ffa6c249b66d16b8
Kevsely <elegis.sossou@yahoo.com>
2022-03-16 06:25:20 +0100

    Fixed the displaying issue of the button inside of collection item Removed shop.data.js file and fixing the consequent issues

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -8,18 +8,22 @@ import './collection-item.styles.scss';
 
 const CollectionItem = ({item, addItem}) => {
     const { imageUrl, name, price } = item
-    return (<div className="collection-item">
+    return (
+    <div className="collection-item">
         <div 
             className="image"
             style={{
                 backgroundImage: `url(${imageUrl})`
             }}/>
+
         <div className="collection-footer">
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
+        
         <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
-    </div>)
+    </div>
+    )
 }
 
 const mapDispatchToProps = dispatch => ({
