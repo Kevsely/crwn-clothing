518fde23d8990ea50cd4537ce8e00ce6aee30325
Kevsely <elegis.sossou@yahoo.com>
2022-03-21 20:01:44 +0100

    Testing a first saga on shop collection fetch

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
-
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
 import CollectionPageContainer from "../collection-page/collection-page.container";
+import { fetchCollectionsStart } from "../../redux/shop/shop.sagas";
 
 class ShopPage extends React.Component {
 
     unsuscribeFromSnapshot = null;
     
     componentDidMount() {
-        this.props.fetchCollectionsStartAsync();
+        this.props.fetchCollectionsStart();
     }
 
     render() {
@@ -27,7 +26,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 })
 
 export default connect(null, mapDispatchToProps)(ShopPage);   
