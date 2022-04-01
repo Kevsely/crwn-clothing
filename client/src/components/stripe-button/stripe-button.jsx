687febf5521cb5cb35e622fecd402d6e1b97b7cd
Kevsely <elegis.sossou@yahoo.com>
2022-04-01 05:43:04 +0100

    A while since the last commit

--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios"
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = "pk_test_51KcWEvFcE4KAuxEPivwhtfJNCNUT33g7P4Msw7EIwU0N8VY0JGA1CGQdN9PcICubUoQCIL1YKwmGQYocAFGG1v5U00hpxwYwVW";
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment sucessful')
+        axios({
+            url: "payment",
+            method: "POST",
+            data: {
+                token,
+                amount: priceForStripe
+            }
+        }).then(response => {
+
+        }).catch(error => {
+            console.log("Payment error: ", JSON.parse(error))
+            alert("There were an issue during the payment. Please, make sure you correctly use the provided credit card")
+        })
     }
 
     return (
